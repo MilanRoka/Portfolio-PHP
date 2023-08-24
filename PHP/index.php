90c7157ae1ddc54cbf7c7c3df9925ca6e0e6a2bf
Milan Roka <101684566+MilanRoka@users.noreply.github.com>
2023-08-24 17:01:17 +0545

    Almost ready

---
+++ PHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="footer.css">
+</head>
+<body>
+    <?php include '../html/navbar.html'; ?>
+    <?php include '../html/home.html'; ?>
+    <?php include '../HTML/about.html';?>
+    <?php include '../HTML/services.html';?>
+    <?php include '../html/contact.html'; ?>
+    <?php include '../html/footer.html'; ?>
+    
+</body>
+</html>
